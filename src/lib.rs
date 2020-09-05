d76ffeb26a75deb0479d2970de9a4168a93c6df9
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2020-09-05 16:19:07 -0700

    Handles command line args more gracefully

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,24 @@ use rand::seq::SliceRandom;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn cal_perms() {
-	let s = std::env::args().nth(1).expect("String must be provided");
-	let mut group = UnicodeSegmentation::graphemes(s.as_str(), true).collect::<Vec<&str>>();
+	if let Some(s) = std::env::args().nth(1) {
+		if s == "--help" {
+			println!("Pass string to get all permutations.");
+			println!("You may want to check if these permutations would be a reasonable size,\ncurrently these produce n! strings, where n is the length of the string.");
+			println!("Note, currently all characters must be distinct.");
+			return;
+		}
+		calculate(&s);
+	}
+	else {
+		println!("Usage `{} str`", std::env::args().nth(0).unwrap());
+		println!("Pass --help for more info");
+		return;
+	}
+}
+
+fn calculate(s: &str) {
+	let mut group = UnicodeSegmentation::graphemes(s, true).collect::<Vec<&str>>();
 	let mut rand = thread_rng();
 
 	let perm_num = (1..=group.len()).product();

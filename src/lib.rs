2fee8219054333e7ac384fae7743b5823062bbbf
MisterEggnog <12803156+MisterEggnog@users.noreply.github.com>
2020-09-12 19:40:16 -0700

    Added LICENSE info

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+/*
+See end of file for license details.
+*/
 use std::io::{self, Write, Result};
 use std::collections::{HashSet, HashMap};
 use rand::prelude::*;
@@ -67,3 +70,20 @@ mod test {
 		assert_eq!(multinomial(&word), 34650);
 	}
 }
+/*
+	gen_perms - Generates all variations of a string.
+    Copyright (C) 2020  Baldwin, Josiah
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-
+/*
+See end of file for license details.
+*/
 mod lib;
 
 fn main() {
 	lib::cal_perms();
 }
+
+/*
+	gen_perms - Generates all variations of a string.
+    Copyright (C) 2020  Baldwin, Josiah
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
